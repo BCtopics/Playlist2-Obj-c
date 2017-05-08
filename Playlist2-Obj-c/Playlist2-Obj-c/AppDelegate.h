83cd7cf74b3c8692562b19e3833f49d9cb1903b2
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-08 10:29:26 -0600

    Cleanup/Fixed file missing error

---
+++ Playlist2-Obj-c/Playlist2-Obj-c/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Playlist2-Obj-c
+//
+//  Created by Bradley GIlmore on 5/8/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Playlist2-Obj-c/Playlist2-Obj-c/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Playlist2-Obj-c
+//
+//  Created by Bradley GIlmore on 5/8/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
